699a7a07a74b941c051c5c1a1583312e78fff1ec
Stuart Scott <stuart.murray.scott@gmail.com>
2019-03-19 13:48:08 -0700

    Add chain reading utils and post api selection

--- source/com/aletheiaware/space/utils/SpaceUtils.java
+++ source/com/aletheiaware/space/utils/SpaceUtils.java
@@ -16,6 +16,8 @@
 
 package com.aletheiaware.space.utils;
 
+import com.aletheiaware.bc.BC.Channel;
+import com.aletheiaware.bc.BC.Channel.RecordCallback;
 import com.aletheiaware.bc.BCProto.Block;
 import com.aletheiaware.bc.BCProto.BlockEntry;
 import com.aletheiaware.bc.BCProto.CompressionAlgorithm;
@@ -27,7 +29,10 @@ import com.aletheiaware.bc.BCProto.SignatureAlgorithm;
 import com.aletheiaware.bc.utils.BCUtils;
 import com.aletheiaware.finance.FinanceProto.Subscription;
 import com.aletheiaware.space.SpaceProto.Meta;
+import com.aletheiaware.space.SpaceProto.Share;
+
 import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
 
 import java.io.File;
 import java.io.IOException;
@@ -50,6 +55,7 @@ import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.RSAPrivateKeySpec;
 import java.security.spec.RSAPublicKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -90,7 +96,7 @@ public final class SpaceUtils {
     public static final String DEFAULT_IMAGE_TYPE = "image/jpeg";
     public static final String DEFAULT_VIDEO_TYPE = "video/mpeg";
 
-    public static final int PREVIEW_IMAGE_SIZE = 64;
+    public static final int PREVIEW_IMAGE_SIZE = 128;
     public static final int PREVIEW_TEXT_LENGTH = 64;
 
     public static final int SOCKET_TIMEOUT = 2 * 60 * 1000;// 2 minutes
@@ -158,6 +164,103 @@ public final class SpaceUtils {
         });
     }
 
+    public static void readMetas(InetAddress host, File cache, String alias, KeyPair keys, byte[] metaRecordHash, RecordCallback callback) throws IOException {
+        final Channel metas = new Channel(META_CHANNEL_PREFIX + alias, BCUtils.THRESHOLD_STANDARD, cache, host);
+        try {
+            metas.sync();
+        } catch (IOException | NoSuchAlgorithmException e) {
+            /* Ignored */
+            e.printStackTrace();
+        }
+        metas.read(alias, keys, metaRecordHash, callback);
+    }
+
+    public static void readFiles(InetAddress host, File cache, String alias, KeyPair keys, byte[] fileRecordHash, RecordCallback callback) throws IOException {
+        final Channel files = new Channel(FILE_CHANNEL_PREFIX + alias, BCUtils.THRESHOLD_STANDARD, cache, host);
+        files.read(alias, keys, fileRecordHash, callback);
+    }
+
+    public static void readShares(InetAddress host, File cache, String alias, KeyPair keys, byte[] shareRecordHash, byte[] metaRecordHash, RecordCallback metaCallback, RecordCallback fileCallback) throws IOException {
+        final Channel shares = new Channel(SHARE_CHANNEL_PREFIX + alias, BCUtils.THRESHOLD_STANDARD, cache, host);
+        try {
+            shares.sync();
+        } catch (IOException | NoSuchAlgorithmException e) {
+            /* Ignored */
+            e.printStackTrace();
+        }
+        shares.read(alias, keys, shareRecordHash, new RecordCallback() {
+            @Override
+            public boolean onRecord(ByteString blockHash, Block block, BlockEntry blockEntry, byte[] key, byte[] payload) {
+                try {
+                    Share share = Share.newBuilder().mergeFrom(payload).build();
+                    // System.out.println("Share: " + share);
+                    Reference sharedMetaReference = share.getMetaReference();
+                    if (metaRecordHash == null || Arrays.equals(sharedMetaReference.getRecordHash().toByteArray(), metaRecordHash)) {
+                        // System.out.println("Shared Reference: " + new String(BCUtils.encodeBase64URL(sharedMetaReference.getRecordHash().toByteArray())));
+                        try {
+                            Block sharedMetaBlock = BCUtils.getBlock(host, sharedMetaReference);
+                            // System.out.println("Shared Block: " + sharedMetaBlock);
+                            if (sharedMetaBlock != null) {
+                                for (BlockEntry sharedMetaBlockEntry : sharedMetaBlock.getEntryList()) {
+                                    // System.out.println("Shared Record: " + sharedMetaBlockEntry.getRecord());
+                                    if (sharedMetaReference.getRecordHash().equals(sharedMetaBlockEntry.getRecordHash())) {
+                                        Record sharedMetaRecord = sharedMetaBlockEntry.getRecord();
+                                        if (metaCallback != null) {
+                                            try {
+                                                byte[] metaKey = share.getMetaKey().toByteArray();
+                                                byte[] decryptedPayload = BCUtils.decryptAES(metaKey, sharedMetaRecord.getPayload().toByteArray());
+                                                metaCallback.onRecord(sharedMetaReference.getBlockHash(), sharedMetaBlock, sharedMetaBlockEntry, metaKey, decryptedPayload);
+                                            } catch (BadPaddingException | IllegalBlockSizeException | InvalidAlgorithmParameterException | InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException ex) {
+                                                /* Ignored */
+                                                ex.printStackTrace();
+                                            }
+                                        }
+                                        if (fileCallback != null) {
+                                            List<ByteString> chunkKeys = share.getChunkKeyList();
+                                            List<Reference> chunkReferences = sharedMetaRecord.getReferenceList();
+                                            int count = Math.min(chunkKeys.size(), chunkReferences.size());
+                                            for (int i = 0; i < count; i++) {
+                                                Reference chunkReference = chunkReferences.get(i);
+                                                try {
+                                                    Block chunkBlock = BCUtils.getBlock(host, chunkReference);
+                                                    if (chunkBlock != null) {
+                                                        for (BlockEntry chunkEntry : chunkBlock.getEntryList()) {
+                                                            if (chunkReference.getRecordHash().equals(chunkEntry.getRecordHash())) {
+                                                                Record chunkRecord = chunkEntry.getRecord();
+                                                                // System.out.println("Shared Chunk: " + chunkRecord);
+                                                                try {
+                                                                    byte[] chunkKey = chunkKeys.get(i).toByteArray();
+                                                                    byte[] decryptedPayload = BCUtils.decryptAES(chunkKey, chunkRecord.getPayload().toByteArray());
+                                                                    fileCallback.onRecord(chunkReference.getBlockHash(), chunkBlock, chunkEntry, chunkKey, decryptedPayload);
+                                                                } catch (BadPaddingException | IllegalBlockSizeException | InvalidAlgorithmParameterException | InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException ex) {
+                                                                    /* Ignored */
+                                                                    ex.printStackTrace();
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                } catch (IOException ex) {
+                                                    /* Ignored */
+                                                    ex.printStackTrace();
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        } catch (IOException e) {
+                            /* Ignored */
+                            e.printStackTrace();
+                        }
+                    }
+                } catch (InvalidProtocolBufferException e) {
+                    e.printStackTrace();
+                }
+                return true;
+            }
+        });
+    }
+
     public static Reference postRecord(String feature, Record record) throws IOException {
         URL url = new URL(SPACE_WEBSITE+"/mining/"+feature);
         HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
@@ -168,46 +271,109 @@ public final class SpaceUtils {
         conn.setRequestProperty("Keep-Alive", "timeout=60000");
         conn.setRequestProperty("Content-Type", PROTOBUF_TYPE);
         conn.setUseCaches(false);
-        try (OutputStream o = conn.getOutputStream()) {
-            record.writeDelimitedTo(o);
-            o.flush();
+        OutputStream out = conn.getOutputStream();
+        record.writeDelimitedTo(out);
+        out.flush();
+        InputStream err = conn.getErrorStream();
+        InputStream in = conn.getInputStream();
+        int response = conn.getResponseCode();
+        System.out.println("Response: " + response);
+        Reference result = null;
+        if (response == HttpsURLConnection.HTTP_OK) {
+            result = Reference.parseDelimitedFrom(in);
+        } else {
+            StringBuilder sb = new StringBuilder();
+            Scanner s = new Scanner(err);
+            while (s.hasNextLine()) {
+                sb.append(s.nextLine());
+                sb.append("\n");
+            }
+            System.err.println("Error: " + sb.toString());
         }
+        if (result == null) {
+            // Disconnect in case of TCP Keep Alive issues
+            out.close();
+            in.close();
+            conn.disconnect();
+        }
+        return result;
+    }
+
+    /**
+     * Register new customer
+     */
+    public static String register(String alias, String email, String paymentId) throws IOException {
+        String params = "api=1&alias=" + URLEncoder.encode(alias, "utf-8")
+                + "&stripeToken=" + URLEncoder.encode(paymentId, "utf-8")
+                + "&stripeEmail=" + URLEncoder.encode(email, "utf-8");
+        System.out.println("Params:" + params);
+        byte[] data = params.getBytes(StandardCharsets.UTF_8);
+        URL url = new URL(SPACE_WEBSITE+"/register");
+        return postForID(url, data);
+    }
+
+    /**
+     * Subscribe customer to Remote Mining Service
+     */
+    public static String subscribe(String alias, String customerId) throws IOException {
+        String params = "api=1&alias=" + URLEncoder.encode(alias, "utf-8")
+                + "&customerId=" + URLEncoder.encode(customerId, "utf-8");
+        System.out.println("Params:" + params);
+        byte[] data = params.getBytes(StandardCharsets.UTF_8);
+        URL url = new URL(SPACE_WEBSITE+"/subscribe");
+        return postForID(url, data);
+    }
+
+    public static String postForID(URL url, byte[] data) throws IOException {
+        HttpsURLConnection conn = postForm(url, data);
+        int response = conn.getResponseCode();
+        System.out.println("Response: " + response);
+        StringBuilder sb = new StringBuilder();
+        if (response == HttpsURLConnection.HTTP_OK) {
+            try (InputStream in = conn.getInputStream()) {
+                Scanner s = new Scanner(in);
+                while (s.hasNextLine()) {
+                    sb.append(s.nextLine());
+                    sb.append("\n");
+                }
+            }
+            return sb.toString();
+        } else {
+            try (InputStream err = conn.getErrorStream()) {
+                Scanner s = new Scanner(err);
+                while (s.hasNextLine()) {
+                    sb.append(s.nextLine());
+                    sb.append("\n");
+                }
+            }
+            System.err.println("Error: " + sb.toString());
+        }
+        return null;
+    }
+
+    public static Reference postForReference(URL url, byte[] data) throws IOException {
+        HttpsURLConnection conn = postForm(url, data);
         int response = conn.getResponseCode();
         System.out.println("Response: " + response);
         if (response == HttpsURLConnection.HTTP_OK) {
             try (InputStream in = conn.getInputStream()) {
                 return Reference.parseDelimitedFrom(in);
             }
+        } else {
+            StringBuilder sb = new StringBuilder();
+            try (InputStream err = conn.getErrorStream()) {
+                Scanner s = new Scanner(err);
+                while (s.hasNextLine()) {
+                    sb.append(s.nextLine());
+                    sb.append("\n");
+                }
+            }
+            System.err.println("Error: " + sb.toString());
         }
         return null;
     }
 
-    /**
-     * Register new customer
-     */
-    public static void register(String alias, String email, String paymentId) throws IOException {
-        String params = "alias=" + URLEncoder.encode(alias, "utf-8")
-                + "&stripeToken=" + URLEncoder.encode(paymentId, "utf-8")
-                + "&stripeEmail=" + URLEncoder.encode(email, "utf-8");
-        System.out.println("Params:" + params);
-        byte[] data = params.getBytes(StandardCharsets.UTF_8);
-        URL url = new URL(SPACE_WEBSITE+"/register");
-        post(url, data);
-    }
-
-    /**
-     * Subscribe customer to Remote Mining Service
-     */
-    public static void subscribe(String alias, String customerId) throws IOException {
-        String params = "alias=" + URLEncoder.encode(alias, "utf-8")
-                + "&customerId=" + URLEncoder.encode(customerId, "utf-8");
-        System.out.println("Params:" + params);
-        byte[] data = params.getBytes(StandardCharsets.UTF_8);
-        URL url = new URL(SPACE_WEBSITE+"/subscribe");
-        post(url, data);
-    }
-
-    public static void post(URL url, byte[] data) throws IOException {
+    public static HttpsURLConnection postForm(URL url, byte[] data) throws IOException {
         HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
         conn.setDoOutput(true);
         conn.setInstanceFollowRedirects(false);
@@ -218,21 +384,11 @@ public final class SpaceUtils {
         conn.setRequestProperty("Keep-Alive", "timeout=60000");
         conn.setRequestProperty("Content-Length", Integer.toString(data.length));
         conn.setUseCaches(false);
-        try (OutputStream o = conn.getOutputStream()) {
-            o.write(data);
-            o.flush();
+        try (OutputStream out = conn.getOutputStream()) {
+            out.write(data);
+            out.flush();
         }
-
-        int response = conn.getResponseCode();
-        System.out.println("Response: " + response);
-        if (response == HttpsURLConnection.HTTP_OK) {
-            try (InputStream in = conn.getInputStream()) {
-                Scanner s = new Scanner(in);
-                while (s.hasNextLine()) {
-                    System.out.println(s.nextLine());
-                }
-            }
-        }
+        return conn;
     }
 
 }
